5be648b2497c45074d48328e9276bce4cc44e453
Raunak Singh <singh.raunak2005@gmail.com>
2025-09-16 10:11:41 +0530

    Done with day41,42

---
+++ src/com/Raunak/Day42/large_print_array.java
@@ -0,0 +1,23 @@
+package com.Raunak.Day42;
+import java.util.*;
+public class large_print_array {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int[] [] m=new int[3][3];
+        for (int i =0;i<3;i++) {
+            for (int j=0;j<3;j++) {
+                m[i][j] = sc.nextInt();
+            }
+        }
+        int large=0;
+        for(int i =0;i<3;i++){
+            large=0;
+            for(int j=0;j<3;j++){
+                if(m[i][j]>large){
+                    large=m[i][j];
+                }
+            }
+            System.out.println("Largest in row "+(i+1)+" "+large);
+        }
+    }
+}
